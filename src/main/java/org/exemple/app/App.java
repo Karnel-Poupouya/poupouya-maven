ca7f3a48ceaf262d3bb1bc1683883db588456793
POUPOUYA <eyemonpoupouya@gmail.com>
2020-02-05 19:26:53 +0000

    first commit

---
+++ src/main/java/org/exemple/app/App.java
@@ -0,0 +1,13 @@
+package org.exemple.app;
+
+/**
+ * Bonjour, j'adore Maven!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bonjour, j'adore Maven !" );
+    }
+}
